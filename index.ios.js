467bb655f6f18e24f0cf0b0527e19ef45c6b7a18
Eric Binnion <ericbinnion@gmail.com>
2015-04-18 18:44:35 -0700

    Beautify

--- index.ios.js
+++ index.ios.js
@@ -5,51 +5,56 @@
 'use strict';
 
 var React = require('react-native'),
-    data = require('./data');
+	data = require('./data');
 var {
-  AppRegistry,
-  StyleSheet,
-  ListView,
-  Text,
-  View,
+	AppRegistry,
+	StyleSheet,
+	ListView,
+	Text,
+	View,
 } = React;
 
 var FreshlyPressed = React.createClass({
-  getInitialState: function() {
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    return {
-      dataSource: ds.cloneWithRows(data.posts),
-    };
-  },
+	getInitialState: function() {
+		var ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+		return {
+			dataSource: ds.cloneWithRows(data.posts),
+		};
+	},
 
-  render: function() {
-    console.log("my data", this.state.dataSource);
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text style={styles.container}>{rowData.title}</Text>}
-      />
-    );
-  },
-});
+	render: function() {
+		return ( < ListView dataSource = {
+				this.state.dataSource
+			}
+			renderRow = {
+				(rowData) => < Text style = {
+					styles.container
+				} > {
+					rowData.title
+				} < /Text>} / >
+			);
+		},
+	});
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 AppRegistry.registerComponent('FreshlyPressed', () => FreshlyPressed);
