a4bc5a01692679ce4d3b708a9db2f50a5f606464
Eric Binnion <ericbinnion@gmail.com>
2015-04-18 18:56:07 -0700

    Factors posts into separate file

--- index.ios.js
+++ index.ios.js
@@ -6,49 +6,24 @@
 
 var React = require('react-native'),
 	data = require('./data');
+
 var {
 	AppRegistry,
 	StyleSheet,
-	ListView,
 	Text,
 	View,
+    NavigatorIOS
 } = React;
 
-var FreshlyPressed = React.createClass({
-	getInitialState: function() {
-		var ds = new ListView.DataSource({
-			rowHasChanged: (r1, r2) => r1 !== r2
-		});
-		return {
-			dataSource: ds.cloneWithRows( data.posts ),
-		};
-	},
+var Posts = require( './posts' );
 
-	renderRow: function( post ) {
-		return (
-			<View style={ styles.row } >
-				<Text>{ post.title }</Text>
-			</View>
-		)
-	},
+var FreshlyPressed = React.createClass({
 
 	render: function() {
 		return (
-			<ListView
-				dataSource = { this.state.dataSource }
-				renderRow = { this.renderRow }
-			/>
+			<Posts />
 		);
 	},
 });
 
-var styles = StyleSheet.create({
-	row: {
-		flex: 1,
-		// justifyContent: 'center',
-		// alignItems: 'center',
-		backgroundColor: '#ffffff',
-	}
-});
-
-AppRegistry.registerComponent('FreshlyPressed', () => FreshlyPressed);
+AppRegistry.registerComponent( 'FreshlyPressed', () => FreshlyPressed );
