a3cb67cf35f187ef265c69cdc1b38efb13381fd9
Eric Binnion <ericbinnion@gmail.com>
2015-04-18 18:56:35 -0700

    Adds missing posts file

---
+++ posts.js
@@ -0,0 +1,49 @@
+var React = require('react-native'),
+	data = require('./data');
+
+var {
+	AppRegistry,
+	StyleSheet,
+	ListView,
+	Text,
+	View
+} = React;
+
+var Posts = React.createClass({
+	getInitialState: function() {
+		var ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+		return {
+			dataSource: ds.cloneWithRows( data.posts ),
+		};
+	},
+
+	renderRow: function( post ) {
+		return (
+			<View style={ styles.row } >
+				<Text>{ post.title }</Text>
+			</View>
+		)
+	},
+
+	render: function() {
+		return (
+			<ListView
+				dataSource = { this.state.dataSource }
+				renderRow = { this.renderRow }
+			/>
+		);
+	},
+});
+
+var styles = StyleSheet.create({
+	row: {
+		flex: 1,
+		// justifyContent: 'center',
+		// alignItems: 'center',
+		backgroundColor: '#ffffff',
+	}
+});
+
+module.exports = Posts;
